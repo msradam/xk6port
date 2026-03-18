f022280d975193823077e5d2d082250a9f26c691
Adam Munawar Rahman <msrahmanadam@gmail.com>
2026-03-18 15:31:04 -0400

    Fix buildenv to match k6port zopen conventions

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -1,11 +1,14 @@
-node('go_1258') {
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/msradam/xk6port.git']]])
+  }
   stage('Build') {
-    build job: 'Port-Pipeline',
-    parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/msradam/xk6port.git'),
-      string(name: 'PORT_DESCRIPTION', value: 'xk6 custom k6 builder for z/OS'),
-      string(name: 'BUILD_LINE', value: 'DEV'),
-      string(name: 'NODE_LABEL', value: 'go_1258')
-    ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/msradam/xk6port.git'), string(name: 'PORT_DESCRIPTION', value: 'xk6 custom k6 builder' ), string(name: 'BUILD_LINE', value: 'DEV'), string(name: 'NODE_LABEL', value: 'go_120' ) ]
   }
 }
